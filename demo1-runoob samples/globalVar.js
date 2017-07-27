0a293265a08f735957bb7ebaff42bff7aafb6fa1
haozhang <haozhang@sysnew.com>
2017-07-27 19:18:46 +0800

    demo1

---
+++ demo1-runoob samples/globalVar.js
@@ -0,0 +1,23 @@
+console.time("获取数据");
+console.log( __filename );
+console.log(__dirname);
+
+var timer = setTimeout(function () {
+    console.log("hello");
+    clearTimeout(timer);
+},5000);
+
+var i = 0;
+var intervalTimer = setInterval(function () {
+    if(i < 5){
+        console.log("hello"+ i);
+        i++;
+    }
+    else{
+        clearInterval(intervalTimer);
+        console.timeEnd('获取数据');
+    }
+},2000);
+
+console.log(global);
+
