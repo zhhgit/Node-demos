1deda6f2f2938ca0c5a569e4557aa08093ac34be
zhhgit <zhh900601@sina.com>
2017-08-04 09:57:18 +0800

    express demo

--- demo2-express sample/server.js
+++ demo2-express sample/server.js
@@ -1,16 +1,33 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
+//静态文件目录，static目录下的直接匹配，http://localhost:3000/img/logo.png匹配图片/static/img/logo.png
+app.use(express.static('static'));
+app.use(cookieParser());
+
+//创建application/x-www-form-urlencoded 编码解析
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.get('/index.html', function (req, res) {
     res.sendFile( __dirname + "/" + "index.html" );
 });
 
-app.get('/process_get', function (req, res) {
+app.get('/getRequest', function (req, res) {
     var response = {
-        first_name:req.query.first_name,
-        last_name:req.query.last_name
+        name:req.query.name,
+        age:req.query.age
     };
     res.end(JSON.stringify(response));
 });
 
-app.listen(8081);
+app.post('/postRequest', urlencodedParser, function (req, res) {
+    var response = {
+        name:req.body.name,
+        age:req.body.age
+    };
+    res.end(JSON.stringify(response));
+});
+
+app.listen(3000);
