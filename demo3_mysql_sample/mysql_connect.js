7f37992aad6a6cff39b40d15b0c115e9f0a98655
zhhgit <zhh900601@sina.com>
2017-08-04 14:50:38 +0800

    demo3 mysql

--- demo3_mysql_sample/mysql_connect.js
+++ demo3_mysql_sample/mysql_connect.js
@@ -5,7 +5,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password : 'zhhpc0601',
+    password : 'XXXXXX',
     database : 'node_practice'
 });
 connection.connect();
