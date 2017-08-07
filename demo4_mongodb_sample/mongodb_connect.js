b2b74fa07b08fdb0269bd6d347945fa8bb742689
zhhgit <zhh900601@sina.com>
2017-08-07 20:37:50 +0800

    demo4 mongodb crud

--- demo4_mongodb_sample/mongodb_connect.js
+++ demo4_mongodb_sample/mongodb_connect.js
@@ -4,6 +4,7 @@
 var MongoClient = require('mongodb').MongoClient;
 var DB_CONN_STR = 'mongodb://localhost:27017/node_practice';
 
+//insert
 var insertData = function(db, callback) {
     var collection = db.collection('person');
     var data = [
@@ -30,9 +31,69 @@ var insertData = function(db, callback) {
     });
 };
 
+//select
+var selectData = function(db, callback) {
+    var collection = db.collection('person');
+    //查询数据
+    var whereStr = {"age":'29'};
+    collection.find(whereStr).toArray(function(err, result) {
+        if(err) {
+            console.log(err);
+            return;
+        }
+        callback(result);
+    });
+};
+
+//update
+var updateData = function(db, callback) {
+    var collection = db.collection('person');
+    var whereStr = {"age":'29'};
+    var updateStr = {$set: { "name" : "zhanghao4" }};
+    collection.update(whereStr,updateStr, function(err, result) {
+        if(err) {
+            console.log(err);
+            return;
+        }
+        callback(result);
+    });
+};
+
+//delete
+var delData = function(db, callback) {
+    var collection = db.collection('person');
+    var whereStr = {"age":'29'};
+    collection.remove(whereStr, function(err, result) {
+        if(err) {
+            console.log(err);
+            return;
+        }
+        callback(result);
+    });
+};
+
 MongoClient.connect(DB_CONN_STR, function(err, db) {
     console.log("连接成功！");
+    //insert
     insertData(db, function(result) {
+        console.log(result);
+        db.close();
+    });
+
+    //update
+    updateData(db, function(result) {
+        console.log(result);
+        db.close();
+    });
+
+    //delete
+    delData(db, function(result) {
+        console.log(result);
+        db.close();
+    });
+
+    //select
+    selectData(db, function(result) {
         console.log(result);
         db.close();
     });
